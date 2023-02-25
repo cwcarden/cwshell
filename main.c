97db65596fc0cd8b19e7d9d847deb2f9818a1126
charles <charles@charlescarden.com>
2023-02-25 11:53:01 -0600

    final release of version 0.1

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 int main(int ac, char **argv)
 {
     char *prompt = "(cwshell-0.1) $ ";
+    char *lineprt;
+    size_t n = 0;
 
     printf("%s", prompt);
+    getline(&lineprt, &n, stdin);
+    printf("%s\n", lineprt);
+
+    free(lineprt);
+
     return 0;
 }
